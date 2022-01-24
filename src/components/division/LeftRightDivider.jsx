06e0d3cbd3803d9aaf85f62f79fbdead2d07ac88
Azeem Sharif <72242160+Azeem-Dev@users.noreply.github.com>
2022-01-24 17:46:18 +0500

    working on UI almost done

--- src/components/division/LeftRightDivider.jsx
+++ src/components/division/LeftRightDivider.jsx
@@ -1,13 +1,13 @@
 const Division = ({ LeftElement, RightElement }) => {
   return (
-    <>
+    <div style={{ display: "flex" }}>
       <div style={{ width: "48%", marginRight: "2%" }}>
         <LeftElement />
       </div>
       <div style={{ width: "47%", marginLeft: "3%" }}>
         <RightElement />
       </div>
-    </>
+    </div>
   );
 };
 export default Division;
--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,10 +1,9 @@
-import { Typography, Input, Select, Divider } from "antd";
-import { PlusCircleFilled } from "@ant-design/icons";
+import { Typography, Input, Select, InputNumber } from "antd";
+import Barcode from "react-barcode";
 import { Types } from "../../utils/constants/types";
 import "./Product.css";
 import Division from "../../components/division/LeftRightDivider";
 import DropDownWithCustomFields from "../../components/dropDownWithCustomFields/DropDownWithCustomFields";
-
 const { Title } = Typography;
 const { Option } = Select;
 const Product = () => {
@@ -16,7 +15,11 @@ const Product = () => {
     <div style={{ margin: "0 20px 0 20px" }}>
       <div className="top-heading-add-product">
         <Title>PRODUCT DETAILS</Title>
-        <div style={{ display: "flex", width: "100%" }}>
+        <div
+          style={{
+            width: "100%",
+          }}
+        >
           <Division
             LeftElement={() => <Input placeholder="Name"></Input>}
             RightElement={() => (
@@ -26,6 +29,53 @@ const Product = () => {
               />
             )}
           />
+          <div style={{ margin: "20px 0 20px 0" }}>
+            <Division
+              LeftElement={() => (
+                <InputNumber
+                  placeholder="Pack Quantity"
+                  min={1}
+                  max={20000}
+                  style={{ width: "100%" }}
+                />
+              )}
+              RightElement={() => (
+                <InputNumber
+                  placeholder="Pack Cost Price"
+                  min={1}
+                  max={20000}
+                  style={{ width: "100%" }}
+                />
+              )}
+            />
+          </div>
+          <div style={{ margin: "20px 0 20px 0" }}>
+            {" "}
+            <Division
+              LeftElement={() => (
+                <InputNumber
+                  placeholder="Pack Retail"
+                  min={1}
+                  max={2000000}
+                  style={{ width: "100%" }}
+                />
+              )}
+              RightElement={() => (
+                <Input placeholder="Rack Number" style={{ width: "100%" }} />
+              )}
+            />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Input placeholder="Manufactorer" style={{ width: "50%" }} />
+            <Barcode value="Azeems Test Barcode" />
+          </div>
         </div>
       </div>
     </div>
